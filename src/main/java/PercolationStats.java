8cf273551ac0741ca8ad03d92dc2048efed10f78
Serhii_Ziniuk <Serhii_Ziniuk@epam.com>
2017-03-05 14:53:38 +0200

    Percolation. clean code

---
+++ src/main/java/PercolationStats.java
@@ -0,0 +1,26 @@
+public class PercolationStats {
+
+    public PercolationStats(int n, int trials) {
+
+    }    // perform trials independent experiments on an n-by-n grid
+
+    public static void main(String[] args) {
+
+    }        // test client (described below)
+
+    public double mean() {
+        return Double.MIN_VALUE;
+    }                          // sample mean of percolation threshold
+
+    public double stddev() {
+        return Double.MIN_VALUE;
+    }                        // sample standard deviation of percolation threshold
+
+    public double confidenceLo() {
+        return Double.MIN_VALUE;
+    }                  // low  endpoint of 95% confidence interval
+
+    public double confidenceHi() {
+        return Double.MIN_VALUE;
+    }                  // high endpoint of 95% confidence interval
+}
