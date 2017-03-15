3dc96cc9c7825f64e639b3970b137997095e6c0c
Serhii_Ziniuk <Serhii_Ziniuk@epam.com>
2017-03-15 15:15:51 +0200

    Deques. Permutation

--- src/main/java/Permutation.java
+++ src/main/java/Permutation.java
@@ -1,53 +1,17 @@
-import java.util.Iterator;
-
 import edu.princeton.cs.algs4.In;
 
 public class Permutation {
     public static void main(String[] args) {
         int k = Integer.valueOf(args[0]);
         In in = new In(args[1]);
-       /* Deque<String> deque = new Deque<String>();
-        while (!in.isEmpty()) {
-            deque.addLast(in.readString());
-        }
 
-        Iterator<String> iterator = deque.iterator();
-        while (iterator.hasNext()) {
-            System.out.println(iterator.next());
-        }
-        System.out.println("Removing ------");
-
-        while (k > 0) {
-            System.out.println(deque.removeFirst());
-            k--;
-        }
-
-        System.out.println("After ------");
-        iterator = deque.iterator();
-        while (iterator.hasNext()) {
-            System.out.println(iterator.next());
-        }*/
-
-        RandomizedQueue<String> deque = new RandomizedQueue<String>();
+        RandomizedQueue<String> deque = new RandomizedQueue<>();
         while (!in.isEmpty()) {
             deque.enqueue(in.readString());
         }
-
-        Iterator<String> iterator = deque.iterator();
-        while (iterator.hasNext()) {
-            System.out.println(iterator.next());
-        }
-        System.out.println("Removing ------");
-
         while (k > 0) {
             System.out.println(deque.dequeue());
             k--;
-        }
-
-        System.out.println("After ------");
-        iterator = deque.iterator();
-        while (iterator.hasNext()) {
-            System.out.println(iterator.next());
         }
     }
 }
